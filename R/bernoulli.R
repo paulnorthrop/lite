89faafb2b3ce38d4e43eaa5099f3d8edd37c5c67
Paul Northrop <p.northrop@ucl.ac.uk>
2022-03-31 21:53:57 +0100

    Tidy

--- R/bernoulli.R
+++ R/bernoulli.R
@@ -1,11 +1,12 @@
 #' Inference for the Bernoulli distribution
 #'
 #' Functions involved in making inferences about the probability of success
-#' in a Bernoulli distribution.
+#' in a Bernoulli distribution using maximum likelihood estimation.
 #'
 #' @param data A numeric vector of outcomes from Bernoulli trials: 0 for a
 #'    failure, 1 for a success.  Alternatively, a logical vector with FALSE
-#'    for a failure and TRUE for a success.
+#'    for a failure and TRUE for a success. Missing values are removed using
+#'   \code{\link[stats:na.fail]{na.omit}}.
 #' @param object A fitted model object returned from \code{fitBernoulli()}.
 #' @param pars A numeric parameter vector of length 1 containing a value of
 #'   the Bernoulli success probability.
@@ -14,23 +15,24 @@
 #'   or \code{\link[sandwich:vcovCL]{meatCL}} (if \code{cluster} is not
 #'   \code{NULL}).
 #' @details
-#' \code{fitBernoulli}: fit a Bernoulli distribution
+#' \code{fitBernoulli}: fit a Bernoulli distribution using maximum likelihood
+#'   estimation.
 #'
-#' \code{logLikVector.bernoulli}: calculates contributions to a log-likelihood
-#' based on the Bernoulli distribution.  The log-likelihood is calculated up to
-#' an additive constant.
+#' \code{logLikVector.bernoulli}: calculates contributions to an independence
+#' log-likelihood based on the Bernoulli distribution.  The log-likelihood is
+#' calculated up to an additive constant.
 #'
 #' \code{nobs, coef, vcov} and \code{logLik} methods are provided.
 #' @return
 #' \code{fitBernoulli} returns an object of class \code{"bernoulli"}, a list
-#' with components: \code{logLik, mle, nobs, vcov, data, obs_data}, where
+#' with components: \code{maxLogLik, mle, nobs, vcov, data, obs_data}, where
 #' \code{data} are the input data and \code{obs_data} are the input data after
 #' any missing values have been removed, using
 #' \code{\link[stats:na.fail]{na.omit}}.
 #'
-#' \code{logLikVector.bernoulli} returns an object of class \code{"logLikVector"}, a
-#' vector length \code{length(data)} containing the likelihood contributions
-#' from the individual observations in \code{data}.
+#' \code{logLikVector.bernoulli} returns an object of class
+#' \code{"logLikVector"}, a vector length \code{length(data)} containing the
+#' likelihood contributions from the individual observations in \code{data}.
 #' @seealso \code{\link[stats]{Binomial}}.  The Bernoulli distribution is the
 #'   special case where \code{size = 1}.
 #' @examples
@@ -53,12 +55,6 @@
 #' coef(fit)
 #' vcov(fit)
 #' logLik(fit)
-#'
-#' # Adjusted log-likelihood
-#' # Create 5 clusters each corresponding approximately to 1 year of data
-#' cluster <- rep(1:5, each = 579)[-1]
-#' afit <- alogLik(fit, cluster = cluster, cadjust = FALSE)
-#' summary(afit)
 #' @name Bernoulli
 NULL
 ## NULL
@@ -75,12 +71,12 @@ fitBernoulli <- function(data) {
   res$vcov <- as.matrix(res$mle * (1 - res$mle) / res$nobs)
   n1 <- sum(obs_data)
   n0 <- res$nobs - n1
-  res$logLik <- n1 * log(res$mle) + n0 * log(1 - res$mle)
-  res$nsuccess <- n1
-  res$nfailure <- n0
+  res$maxLogLik <- n1 * log(res$mle) + n0 * log(1 - res$mle)
   class(res) <- "Bernoulli"
   return(res)
 }
+
+# Methods for class "Bernoulli"
 
 #' @rdname Bernoulli
 #' @export
@@ -133,7 +129,7 @@ vcov.Bernoulli <- function(object, ...) {
 #' @rdname Bernoulli
 #' @export
 logLik.Bernoulli <- function(object, ...) {
-  val <- object$logLik
+  val <- object$maxLogLik
   attr(val, "nobs") <- nobs(object)
   attr(val, "df") <- length(coef(object))
   class(val) <- "logLik"
