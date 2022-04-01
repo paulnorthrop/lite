3ede244ba880e97e34dd3fd3d8036a37e91ddfaf
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-01 18:52:22 +0100

    Add plot method

--- R/lite_methods.R
+++ R/lite_methods.R
@@ -9,7 +9,7 @@
 #' @param ... Additional optional arguments. At present no optional
 #'   arguments are used.
 #' @return A numeric vector of length 4 with names
-#'   \code{c("p_u", "sigma_u", "xi", "theta")}.
+#'   \code{c("p[u]", "sigma[u]", "xi", "theta")}.
 #' @export
 coef.lite <- function(object, ...) {
   if (!inherits(object, "lite")) {
@@ -19,7 +19,7 @@ coef.lite <- function(object, ...) {
   gfit <- attr(object, "gp")
   kfit <- attr(object, "kgaps")
   cf <- c(attr(bfit, "MLE"), attr(gfit, "MLE"), kfit$theta)
-  names(cf) <- c("p_u", "sigma_u", "xi", "theta")
+  names(cf) <- c("p[u]", "sigma[u]", "xi", "theta")
   return(cf)
 }
 
@@ -35,7 +35,7 @@ coef.lite <- function(object, ...) {
 #' @param ... Additional optional arguments. At present no optional
 #'   arguments are used.
 #' @return A 4 by 4 numeric matrix with row and column names
-#'   \code{c("p_u", "sigma_u", "xi", "theta")}.
+#'   \code{c("p[u]", "sigma[u]", "xi", "theta")}.
 #' @export
 vcov.lite <- function(object, ...) {
   if (!inherits(object, "lite")) {
@@ -48,7 +48,7 @@ vcov.lite <- function(object, ...) {
   vc[1, 1] <- attr(bfit, "adjVC")
   vc[2:3, 2:3] <- attr(gfit, "adjVC")
   vc[4, 4] <- vcov(kfit)
-  vc_names <- c("p_u", "sigma_u", "xi", "theta")
+  vc_names <- c("p[u]", "sigma[u]", "xi", "theta")
   dim(vc) <- c(length(vc_names), length(vc_names))
   dimnames(vc) <- list(vc_names, vc_names)
   return(vc)
@@ -66,7 +66,7 @@ vcov.lite <- function(object, ...) {
 #' @param ... Additional optional arguments. At present no optional
 #'   arguments are used.
 #' @return A numeric vector of length 3 with names
-#'   \code{c("p_u", "gp", "theta")}.
+#'   \code{c("p[u]", "gp", "theta")}.
 #'
 #' @export
 nobs.lite <- function(object,  ...) {
@@ -77,7 +77,7 @@ nobs.lite <- function(object,  ...) {
   gnobs <- attr(attr(object, "gp"), "nobs")
   knobs <-  attr(object, "kgaps")$ss$n_kgaps
   n <- c(bnobs, gnobs, knobs)
-  names(n) <- c("p_u", "gp", "theta")
+  names(n) <- c("p[u]", "gp", "theta")
   return(n)
 }
 
@@ -111,7 +111,7 @@ logLik.lite <- function(object, ...) {
   gloglik <- attr(gfit, "max_loglik")
   kloglik <- kfit$max_loglik
   val <- bloglik + gloglik + kloglik
-  attr(val, "nobs") <- nobs(y)
+  attr(val, "nobs") <- nobs(object)
   attr(val, "df") <- 4
   class(val) <- "logLik"
   return(val)
@@ -129,7 +129,7 @@ logLik.lite <- function(object, ...) {
 #'   arguments are used.
 #' @return An object containing the original function call and a matrix of
 #'   estimates and estimated standard errors with row names
-#'   \code{c("p_u", "sigma_u", "xi", "theta")}.  The object is printed by
+#'   \code{c("p[u]", "sigma[u]", "xi", "theta")}.  The object is printed by
 #'   \code{\link{print.summary.lite}}.
 #' @section Examples:
 #' See the examples in \code{\link{flite}}.
@@ -170,3 +170,99 @@ print.summary.lite <- function(x, ...) {
   print(x$matrix, ...)
   invisible(x)
 }
+
+
+# ================================= plot.lite =============================== #
+
+#' Plot method for objects of class \code{"lite"}
+#'
+#' \code{print} method for an object \code{x} of class \code{"lite"}.
+#'
+#' @param x An object of class "lite", a result of a call to
+#'   \code{\link{flite}}.
+#' @param which A character scalar indicating which plot(s) to produce.
+#'   If \code{which = "all"} then all 4 plots described in \strong{Details}
+#'   are produced.  Otherwise, only one of these plots is produced, with the
+#'   possible names of the arguments being in the order that the plots are
+#'   described in \strong{Details}.
+#' @param adj_type A character scalar passed to
+#'   \code{\link[chandwich]{conf_intervals}} and
+#'   \code{\link[chandwich]{conf_region}} as the argument \code{type} to select
+#'   the type of adjustment applied to the independence log-likelihood.  Of the
+#'   3 adjustments, \code{"vertical"} is preferred because it preserves
+#'   constraints on the parameters, whereas the \code{"cholesky"} and
+#'   \code{"spectral"} adjustment do not.  In the generalised Pareto case the
+#'   constraint that \eqn{\xi > - \sigma_u / x_{(n)}}{\xi > \sigma_u / x_(n)},
+#'   where \eqn{x_{(n)}}{x_(n)} is the largest excesses of the threshold \eqn{u},
+#'   is preserved.
+#' @param ... Arguments to be passed to \code{\link[stats]{plot}}, such as
+#'   graphical parameters.
+#' @details If \code{which = "all"} then 4 plots are produced.
+#'     \itemize{
+#'       \item{Top left: (adjusted) log-likelihood for the threshold exceedence
+#'         probability \eqn{p_u}, with a horizontal line indicating a
+#'         95\% confidence interval for \eqn{p_u}.}
+#'       \item{Top right: contour plot of the (adjusted) log-likelihood for the
+#'         GP parameters \eqn{(\sigma_u, \xi)}, showing
+#'         (25, 50, 75, 90, 95)\% confidence regions. The linear constraint
+#'         \eqn{\xi > - \sigma_u / x_{(n)}}{\xi > \sigma_u / x_(n)} is drawn
+#'         on the plot.}
+#'       \item{Bottom left: (adjusted) log-likelihood for \eqn{\xi}, with a
+#'         horizontal line indicating a 95\% confidence interval for \eqn{\xi}.}
+#'       \item{Bottom right: log-likelihood for the extremal index \eqn{\theta},
+#'         with a horizontal line indicating a 95\% confidence interval for
+#'         \eqn{\theta}.}
+#'     }
+#' @section Examples:
+#' See the examples in \code{\link{flite}}.
+#' @export
+plot.lite <- function(x, which = c("all", "pu", "gp", "xi", "theta"),
+                      adj_type = c("vertical", "none", "cholesky", "spectral"),
+                      ...) {
+  if (!inherits(x, "lite")) {
+    stop("use only with \"lite\" objects")
+  }
+  adj_type <- match.arg(adj_type)
+  which <- match.arg(which)
+  if (which == "all") {
+    which <- c("pu", "gp", "xi", "theta")
+    oldpar <- graphics::par(mfrow = c(2, 2))
+  } else {
+    oldpar <- graphics::par(mfrow = c(1, 1))
+  }
+  on.exit(graphics::par(oldpar))
+  # Bernoulli (p[u])
+  if ("pu" %in% which) {
+    ci <- chandwich::conf_intervals(attr(x, "bernoulli"), type = adj_type)
+    bplot <- function(obj, ..., xlab = expression(p[u])) {
+      plot(obj, ..., xlab = xlab)
+    }
+    bplot(ci, ...)
+  }
+  # GP - to do, perhaps plot.confreg
+  if ("gp" %in% which) {
+    gp <- attr(x, "gp")
+    cr <- chandwich::conf_region(gp, type = adj_type)
+    gpplot <- function(obj, ..., conf = c(25, 50, 75, 90, 95)) {
+      plot(obj, ..., conf = conf)
+    }
+    gpplot(cr, ...)
+    ofit <- attr(gp, "original_fit")
+    m <- max(ofit$exceedances) - ofit$threshold
+    abline(a = 0, b = -1 / m)
+  }
+  # GP shape parameter xi
+  if ("xi" %in% which) {
+    ci_xi <- chandwich::conf_intervals(gp, which_pars = "xi", type = adj_type)
+    plot(ci_xi, ...)
+  }
+  # K-gaps for theta
+  if ("theta" %in% which) {
+    tplot <- function(obj, ..., main = "") {
+      plot(obj, ..., main = main)
+    }
+    tplot(confint(attr(x, "kgaps")), ...)
+  }
+  return(invisible())
+}
+
