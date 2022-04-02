223e7316f83b8663f5d98a6591b269e7f220b438
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-02 02:38:08 +0100

    Refs

--- R/lite.R
+++ R/lite.R
@@ -9,15 +9,12 @@
 #'
 #' See \code{vignette("lite-vignette", package = "lite")} for an overview of the
 #' package.
+#' @references Northrop, P. J. and Chandler, R. E. (2021).
+#'   chandwich: Chandler-Bate Sandwich Loglikelihood Adjustment. R package
+#'   version 1.1.5. \url{https://CRAN.R-project.org/package=chandwich}.
 #' @references Northrop, P. J. and Christodoulides, C. (2022). exdex:
 #' Estimation of the Extremal Index. R package version 1.1.1.
 #' \url{https://CRAN.R-project.org/package=exdex/}.
-#' @references Northrop, P. J. and Yin, C. (2022). lax: Loglikelihood Adjustment
-#' for Extreme Value Models. R package version 1.2.0.
-#' \url{https://CRAN.R-project.org/package=lax}
-#' @references  Northrop, P. J. (2020). revdbayes: Ratio-of-Uniforms Sampling
-#' for Bayesian Extreme Value Analysis. R package version 1.3.9.
-#' \url{https://paulnorthrop.github.io/revdbayes/}
 #' @docType package
 #' @name lite
 #' @import sandwich
