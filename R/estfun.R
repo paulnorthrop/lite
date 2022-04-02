87d8eb1fde26ccd1f0028b9d9a80bc716a40a0a9
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-02 18:28:58 +0100

    Algebraic score function for GP

--- R/estfun.R
+++ R/estfun.R
@@ -12,8 +12,27 @@ estfun.default <- function(x, loglik_fn, ...) {
 # GP
 
 #' @export
-estfun.GP <- function(x, loglik_fn, ...) {
-  U <- numDeriv::jacobian(loglik_fn, x = coef(x), ...)
+estfun.GP <- function(x, eps = 1e-5, ...) {
+  print("Using my code")
+  pars <- coef(x)
+  sigma <- pars[1]
+  xi <- pars[2]
+  z <- xi / sigma
+  # Threshold excesses
+  y <- x$exceedances - x$threshold
+  t0 <- 1 + z * y
+  U <- matrix(NA, nrow = length(y), ncol = 2)
+  U[, 1] <- -1 / sigma + (xi + 1) * y / (t0 * sigma ^ 2)
+  if (abs(xi) < eps) {
+    s1 <- 1 / 2
+    s2 <- 2 * z * y / 3
+    s3 <- 3 * z ^ 2 * y ^ 2 / 4
+    U[, 2] <- y ^ 2 * (s1 - s2 + s3) / sigma ^ 2 - y / (sigma * t0)
+  } else {
+    t1 <- log(t0) / z ^ 2
+    t2 <- y / (z * t0)
+    U[, 2] <- (t1 - t2) / sigma ^ 2 - y / (sigma * t0)
+  }
   colnames(U) <- names(coef(x))
   return(U)
 }
