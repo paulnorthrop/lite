e109ee95d05c614bd32fc347d9b57e63dc3fb34e
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-04 17:35:06 +0100

    Merge logLikVector docs

--- R/logLikVector.R
+++ R/logLikVector.R
@@ -1,22 +1,28 @@
+#' Functions for log-likelihoood contributions
+#'
+#' @name logLikVector
+NULL
+## NULL
+
 #' Evaluate log-likelihood contributions from specific observations
 #'
 #' Generic function for calculating log-likelihood contributions from
 #' individual observations for a fitted model.
 #'
-#' @param object A fitted model object.
+#' @param x A fitted model object.
 #' @param ... Further arguments.
+#' @rdname logLikVector
 #' @export
-logLikVector <- function(object, ...) {
+logLikVector <- function(x, ...) {
   UseMethod("logLikVector")
 }
 
 #' Sum log-likelihood contributions from individual observations
 #'
-#' S3 logLik method for logLikVector objects
-#'
-#' @param object An object of class \code{"logLikVector"} return from a
+#' @param object An object of class \code{"logLikVector"} returned from a
 #'   \code{logLikVector} method.
 #' @param ... Further arguments.
+#' @rdname logLikVector
 #' @export
 logLik.logLikVector <- function(object, ...) {
   save_attributes <- attributes(object)
