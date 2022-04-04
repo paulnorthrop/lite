247939f63806c987d5f00174ae0d08d447a7132e
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-04 23:05:32 +0100

    Add test

--- tests/testthat/test-returnLevel.R
+++ tests/testthat/test-returnLevel.R
@@ -1,7 +1,9 @@
 #context("returnLevel")
 
 # Check that an error is thrown if we ask plot.returnLevel to find a confidence
-# interval that has a higher level of confidence than one already calculated
+# interval that has a higher level of confidence than one already calculated.
+# Also check that if we ask for a lower level then the confidence interval
+# is shorter.
 
 got_exdex <- requireNamespace("exdex", quietly = TRUE)
 
@@ -13,7 +15,21 @@ if (got_exdex) {
   cfit <- flite(cdata, u = 45, k = 3)
   rl <- returnLevel(cfit, inc = 5, ny = 31 * 24)
 
+  # Throw error
   test_that("plot.returnLevel error case", {
     testthat::expect_error(plot(rl, level = 0.99))
+  })
+
+  # Shorter interval
+  oldrl <- rl$rl_prof
+  newrl <- plot(rl, level = 0.9)
+  test_that("old vs new RL: MLEs agree", {
+    testthat::expect_equal(oldrl["mle"], newrl["mle"])
+  })
+  test_that("old vs new RL: new lower is higher", {
+    testthat::expect_gt(newrl["lower"], oldrl["lower"])
+  })
+  test_that("old vs new RL: new upper is lower", {
+    testthat::expect_lt(newrl["upper"], oldrl["upper"])
   })
 }
