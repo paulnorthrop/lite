b4fff9ac556bf9b3bd048b6725175a524d36b920
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-04 22:37:01 +0100

    Test returnLevel

---
+++ tests/testthat/test-returnLevel.R
@@ -0,0 +1,19 @@
+#context("returnLevel")
+
+# Check that an error is thrown if we ask plot.returnLevel to find a confidence
+# interval that has a higher level of confidence than one already calculated
+
+got_exdex <- requireNamespace("exdex", quietly = TRUE)
+
+if (got_exdex) {
+  ### Cheeseboro wind gusts
+
+  # Fit model
+  cdata <- exdex::cheeseboro
+  cfit <- flite(cdata, u = 45, k = 3)
+  rl <- returnLevel(cfit, inc = 2.5, ny = 31 * 24)
+
+  test_that("plot.returnLevel error case", {
+    testthat::expect_error(plot(rl, level = 0.99))
+  })
+}
