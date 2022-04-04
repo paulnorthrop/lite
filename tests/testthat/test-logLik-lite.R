5ccc82e4e2e6fb40e8fd21e82a70e73754faac2e
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-04 14:31:20 +0100

    Check for exdex

--- tests/testthat/test-logLik-lite.R
+++ tests/testthat/test-logLik-lite.R
@@ -4,18 +4,21 @@
 # the values of max_loglik for the 3 constituent models gives the same value
 # as the value of the (adjusted) log-likelihood evaluated at the MLE
 
+got_exdex <- requireNamespace("exdex", quietly = TRUE)
 
-### Cheeseboro wind gusts
+if (got_exdex) {
+  ### Cheeseboro wind gusts
 
-# Make inferences
-cdata <- exdex::cheeseboro
-cfit <- flite(cdata, u = 45, k = 3)
-summary(cfit)
+  # Make inferences
+  cdata <- exdex::cheeseboro
+  cfit <- flite(cdata, u = 45, k = 3)
+  summary(cfit)
 
-# 2 ways to find the maximised log-likelihood value
-res1 <- logLik(cfit)
-res2 <- check_logLik_lite(cfit)
+  # 2 ways to find the maximised log-likelihood value
+  res1 <- logLik(cfit)
+  res2 <- check_logLik_flite(cfit)
 
-test_that("Fitted object and summary() agree", {
-  testthat::expect_equal(res1, res2, ignore_attr = TRUE)
-})
+  test_that("Fitted object and summary() agree", {
+    testthat::expect_equal(res1, res2, ignore_attr = TRUE)
+  })
+}
