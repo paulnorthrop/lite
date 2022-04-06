b3b63f840115c3f2f6e1654a71864e00f931cd62
Paul Northrop <p.northrop@ucl.ac.uk>
2022-04-06 15:06:52 +0100

    Document logLikVector properly

--- R/logLikVector.R
+++ R/logLikVector.R
@@ -1,27 +1,102 @@
 #' Functions for log-likelihood contributions
 #'
+#' Generic function for calculating log-likelihood contributions from
+#' individual observations for a fitted model object.
+#'
+#' @param object A fitted model object.
+#' @param ... Further arguments. None are currently used for
+#'   \code{logLikVector.Bernoulli} or \code{logLikVector.GP}.
+#' @details A \code{logLikVector} method is used to construct a log-likelihood
+#'   function to supply as the argument \code{loglik} to the function
+#'   \code{\link[chandwich]{adjust_loglik}}, which performs log-likelihood
+#'   adjustment for parts 1 and 2 of the inferences performed by
+#'   \code{\link{flite}}.
+#'
+#'   The \code{logLik} method \code{logLik.LogLikVector} sums the
+#'   log-likelihood contributions from individual observations.
+#' @return For \code{logLikVector}: an object of class \code{logLikVec}.
+#'   This is a numeric vector of length \eqn{n} containing contributions to the
+#'   score function from \eqn{n} observations, with attributes \code{"df"}
+#'   (degrees of freedom), giving the number of estimated parameters in the
+#'   model, and \code{"nobs"}, giving the number observations used to perform
+#'   the estimation.
+#'
+#'   For \code{logLik.logLikVector}: an object of class \code{logLik}.  This is
+#'   a number with the attributes \code{"df"} and \code{"nobs"} as described
+#'   above.
+#' @seealso \code{\link{Bernoulli}} for maximum likelihood inference for the
+#'   Bernoulli distribution.
+#' @seealso \code{\link{generalisedPareto}} for maximum likelihood inference
+#'   for the generalised Pareto distribution.
+#' @examples
+#' # logLikVector.Bernoulli
+#' bfit <- fitBernoulli(c(exdex::cheeseboro) > 45)
+#' bvec <- logLikVector(bfit)
+#' logLik(bvec)
+#' logLik(bfit)
+#'
+#' # estfun.generalisedPareto
+#' gpfit <- fitGP(c(exdex::cheeseboro), u = 45)
+#' gpvec <- logLikVector(gpfit)
+#' logLik(gpvec)
+#' logLik(gpfit)
 #' @name logLikVector
 NULL
 ## NULL
 
-#' Evaluate log-likelihood contributions from specific observations
-#'
-#' Generic function to calculate log-likelihood contributions from
-#' individual observations for a fitted model object.
-#'
-#' @param object A fitted model object.
-#' @param ... Further arguments.
 #' @rdname logLikVector
 #' @export
 logLikVector <- function(object, ...) {
   UseMethod("logLikVector")
 }
 
-#' Sum log-likelihood contributions from individual observations
-#'
-#' @param object An object of class \code{"logLikVector"} returned from a
-#'   \code{logLikVector} method.
-#' @param ... Further arguments.
+#' @rdname logLikVector
+#' @export
+logLikVector.Bernoulli <- function(object, pars = NULL, ...) {
+  # If the parameter estimates have not been provided in pars then extract
+  # them from the fitted object
+  if (is.null(pars)) {
+    pars <- coef(object)
+  }
+  n_pars <- length(pars)
+  prob <- pars[1]
+  if (prob < 0 || prob > 1) {
+    val <- -Inf
+  } else {
+    val <- stats::dbinom(object$obs_data, 1, prob, log = TRUE)
+  }
+  # Return the usual attributes for a "logLik" object
+  attr(val, "nobs") <- nobs(object)
+  attr(val, "df") <- n_pars
+  class(val) <- "logLikVector"
+  return(val)
+}
+
+#' @rdname logLikVector
+#' @export
+logLikVector.GP <- function(object, pars = NULL, ...) {
+  # If the parameter estimates have not been provided in pars then extract
+  # them from the fitted object
+  if (is.null(pars)) {
+    pars <- coef(object)
+  }
+  n_pars <- length(pars)
+  sigma <- pars[1]
+  xi <- pars[2]
+  # Calculate the log-likelihood contributions
+  if (any(sigma <= 0)) {
+    val <- -Inf
+  } else {
+    val <- revdbayes::dgp(x = object$exceedances, loc = object$threshold,
+                          scale = sigma, shape = xi, log = TRUE)
+  }
+  # Return the usual attributes for a "logLik" object
+  attr(val, "nobs") <- nobs(object)
+  attr(val, "df") <- n_pars
+  class(val) <- "logLikVector"
+  return(val)
+}
+
 #' @rdname logLikVector
 #' @export
 logLik.logLikVector <- function(object, ...) {
